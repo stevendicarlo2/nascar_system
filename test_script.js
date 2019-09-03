469621f0ed3ccf350da7f29225a7786a6301f06a
Steven DiCarlo <stevendicarlo2@gmail.com>
2019-09-02 21:47:42 -0700

    Improve weekly breakdown table

--- test_script.js
+++ test_script.js
@@ -19,20 +19,36 @@ if (!window.tableContentsHash) {
   window.tableContentsHash = 0;
 }
 
+function removeWeeklyBreakdownTable() {
+  let existingTable = document.querySelector(".tableRoot");
+  if (existingTable) {
+    existingTable.parentNode.removeChild(existingTable);
+  }
+}
+
 function createWeeklyBreakdownTable(scoreData, pointsPerWin) {
-  let defaultTable = document.querySelector(".Table2__tbody");
-  let base = defaultTable.parentNode.parentNode;
+  removeWeeklyBreakdownTable();
+  let doubleTableBase = document.querySelector(".h2hTables");
+  let base;
+  if (doubleTableBase) {
+    base = doubleTableBase;
+  } else {
+    let defaultTable = document.querySelector(".Table2__tbody");
+    base = defaultTable.parentNode.parentNode;
+  }
   // let shadowHost = document.createElement("div");
   // shadowHost.id = "shadowHost";
   // base.appendChild(shadowHost);
   // let shadowRoot = shadowHost.attachShadow({ mode: "open"});
   let shadowRoot = document.createElement("div");
+  shadowRoot.classList.add("tableRoot");
   base.appendChild(shadowRoot);
   shadowRoot.innerHTML = '<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">';
   // shadowRoot.innerHTML += '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">';
   // shadowRoot.innerHTML += '<link href="datatables/mdb.min.css" rel="stylesheet">';
   // shadowRoot.innerHTML += '<link href="datatables/datatables.min.css" rel="stylesheet">';
   
+  // shadowRoot.innerHTML += '<script src="//cdnjs.cloudflare.com/ajax/libs/d3/3.4.11/d3.min.js"></script>';
   shadowRoot.innerHTML += '<script type="text/javascript" src="datatables/jquery-3.4.1.min.js"></script>';
   shadowRoot.innerHTML += '<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>';
   shadowRoot.innerHTML += '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>';
@@ -54,6 +70,14 @@ function createWeeklyBreakdownTable(scoreData, pointsPerWin) {
   ownerCol.classList.add("th-sm");
   ownerCol.innerHTML = "Team";
   headRow.appendChild(ownerCol);
+  let totalCol = document.createElement("th");
+  totalCol.classList.add("th-sm");
+  totalCol.innerHTML = "Total NP";
+  headRow.appendChild(totalCol);
+  let adjCol = document.createElement("th");
+  adjCol.classList.add("th-sm");
+  adjCol.innerHTML = "Total ANP";
+  headRow.appendChild(adjCol);
   for (let week in scoreData.weekly_breakdown) {
     if (week === "storedTime") {
       continue;
@@ -63,15 +87,6 @@ function createWeeklyBreakdownTable(scoreData, pointsPerWin) {
     weekCol.innerHTML = "Week " + (parseInt(week) + 1).toString();
     headRow.appendChild(weekCol);
   }
-  let totalCol = document.createElement("th");
-  totalCol.classList.add("th-sm");
-  totalCol.innerHTML = "Total NP";
-  headRow.appendChild(totalCol);
-  let adjCol = document.createElement("th");
-  adjCol.classList.add("th-sm");
-  adjCol.innerHTML = "Total ANP";
-  headRow.appendChild(adjCol);
-
 
   // Filling with data
   let tbody = document.createElement("tbody");
@@ -79,36 +94,149 @@ function createWeeklyBreakdownTable(scoreData, pointsPerWin) {
   for (let team in scoreData.totals) {
     let row = document.createElement("tr");
     let nameEntry = document.createElement("td");
+    nameEntry.classList.add("teamName");
     nameEntry.innerHTML = team;
     row.appendChild(nameEntry);
+    let totEntry = document.createElement("td");
+    totEntry.classList.add("NP_data");
+    totEntry.innerHTML = "<b>" + scoreData.totals[team].total_NP + "</b>";
+    row.appendChild(totEntry);
+    let adjEntry = document.createElement("td");
+    adjEntry.classList.add("ANP_data");
+    adjEntry.innerHTML = "<b>" + (scoreData.totals[team].total_NP + scoreData.totals[team].wins*pointsPerWin) + "</b>";
+    row.appendChild(adjEntry);
+    tbody.appendChild(row);
     for (let week in scoreData.weekly_breakdown) {
       if (week === "storedTime") {
         continue;
       }
       let info = scoreData.weekly_breakdown[week][team];
       let entry = document.createElement("td");
-      entry.innerHTML = info.nascar_points + info.wins*pointsPerWin;
-      if (info.wins === 1) {
-        entry.innerHTML = "<u>" + entry.innerHTML + "</u>";
-      }
+      entry.setAttribute("nascar-points", info.nascar_points + info.wins*pointsPerWin);
+      entry.setAttribute("raw-points", info.score);
+      entry.setAttribute("wins", info.wins);
       row.appendChild(entry);
     }
-    let totEntry = document.createElement("td");
-    totEntry.innerHTML = "<b>" + scoreData.totals[team].total_NP + "</b>";
-    row.appendChild(totEntry);
-    let adjEntry = document.createElement("td");
-    adjEntry.innerHTML = "<b>" + (scoreData.totals[team].total_NP + scoreData.totals[team].wins*pointsPerWin) + "</b>";
-    row.appendChild(adjEntry);
-    tbody.appendChild(row);
   }
+  shadowRoot.innerHTML += '<label id="scoreTypeSwitchLabel" for="scoreTypeSwitch">Nascar Points</label><label class="switch"><input id="scoreTypeSwitch" type="checkbox" checked><span class="slider round"></span></label>';
   
 
-
   $(document).ready(function () {
-    $('#weekly_breakdown_table').DataTable();
+    fillTableWithData(true);
+    if ($.fn.dataTable.isDataTable('#weekly_breakdown_table')) {
+      $('#weekly_breakdown_table').DataTable();
+    } else {
+      $('#weekly_breakdown_table').DataTable({
+        "searching": false,
+        "paging": false,
+        "order": [[ 2, "desc" ]]
+      });
+    }
     $('.dataTables_length').addClass('bs-select');
+    $('#weekly_breakdown_table').parent().addClass('freezeTable');
+    var freezeTable = new FreezeTable('.freezeTable', {"columnNum": 3, "columnWrapStyles": {'border-right': '3px solid black'}});
+    $('#weekly_breakdown_table th').click(function() {
+      freezeTable.update();
+    });
+    $("#scoreTypeSwitch").click(function() {
+      $("#scoreTypeSwitchLabel").html(this.checked ? "Nascar Points" : "Weekly Score");
+      fillTableWithData(this.checked);
+      freezeTable.update();
+    });
+  });
+}
+
+function fillTableWithData(useNascarPoints) {
+  $(document).ready(function () {
+    $("#weekly_breakdown_table td").each(function(i) {
+      if (!(this.classList.contains("NP_data") || this.classList.contains("ANP_data") || this.classList.contains("teamName"))) {
+        if (useNascarPoints) {
+          this.innerHTML = this.getAttribute("nascar-points");
+        } else {
+          this.innerHTML = this.getAttribute("raw-points");
+        }
+        if (this.getAttribute("wins") === "1") {
+          this.innerHTML = "<u>" + this.innerHTML + "</u>";
+        }
+      }
+    });
+    highlightTable();
+  });
+}
+
+function highlightTable() {
+  let minScore, minNP, minANP, minWeekScore;
+  let maxScore=0, maxNP=0, maxANP=0, maxWeekScore=0;
+
+  d3.selectAll("#weekly_breakdown_table td").each(function(d, i) {
+    if (this.classList.contains("NP_data")) {
+      let nascarPoints = parseFloat(this.textContent);
+      if (minNP === undefined) {
+        minNP = nascarPoints;
+      } else {
+        minNP = (nascarPoints < minNP) ? nascarPoints : minNP;
+      }
+      maxNP = (nascarPoints > maxNP) ? nascarPoints : maxNP;
+    } else if (this.classList.contains("ANP_data")) {
+      let adjNascarPoints = parseFloat(this.textContent);
+      if (minANP === undefined) {
+        minANP = adjNascarPoints;
+      } else {
+        minANP = (adjNascarPoints < minANP) ? adjNascarPoints : minANP;
+      }
+      maxANP = (adjNascarPoints > maxANP) ? adjNascarPoints : maxANP;
+    } else if (!this.classList.contains("teamName")) {
+      let nascarPoints = parseFloat(this.getAttribute("nascar-points"));
+      let rawPoints = parseFloat(this.getAttribute("raw-points"));
+      if (minScore === undefined) {
+        minScore = rawPoints;
+      } else {
+        minScore = (rawPoints < minScore) ? rawPoints : minScore;
+      }
+      maxScore = (rawPoints > maxScore) ? rawPoints : maxScore;
+
+      if (minWeekScore === undefined) {
+        minWeekScore = nascarPoints;
+      } else {
+        minWeekScore = (nascarPoints < minWeekScore) ? nascarPoints : minWeekScore;
+      }
+      maxWeekScore = (nascarPoints > maxWeekScore) ? nascarPoints : maxWeekScore;
+    }
   });
 
+  let badColor = "#fe7575";
+  let mediumColor = "#ffff88";
+  let goodColor = "#64a764";
+  let colorScale = d3.scaleLinear()
+  .domain([1, (maxWeekScore+1)/2, maxWeekScore])
+  .range([badColor, mediumColor, goodColor]);
+  let colorScaleNP = d3.scaleLinear()
+  .domain([minNP, (minNP + maxNP)/2, maxNP])
+  .range([badColor, mediumColor, goodColor]);
+  let colorScaleANP = d3.scaleLinear()
+  .domain([minANP, (minANP+maxANP)/2, maxANP])
+  .range([badColor, mediumColor, goodColor]);
+  let colorScaleRaw = d3.scaleLinear()
+  .domain([minScore, (minScore+maxScore)/2, maxScore])
+  .range([badColor, mediumColor, goodColor]);
+
+  d3.selectAll("#weekly_breakdown_table td").each(function(d, i) {
+    let value = parseFloat(this.textContent);
+    let color;
+    if (this.classList.contains("NP_data")) {
+      color = colorScaleNP(value);
+    } else if (this.classList.contains("ANP_data")) {
+      color = colorScaleANP(value);
+    } else if (!this.classList.contains("teamName")) {
+      if ($("#scoreTypeSwitch").is(":checked")) {
+        color = colorScale(value);
+      } else {
+        console.log(value);
+        color = colorScaleRaw(value);
+      }
+    }
+    this.style.backgroundColor = color;
+  });
 }
 
 async function showNascarDataWhenReady() {
