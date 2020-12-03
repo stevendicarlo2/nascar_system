ca3bf5dab133f4b9a0c8ce1164d0f29696ad53e6
Steven DiCarlo <stdicarl@microsoft.com>
2020-12-02 21:31:13 -0800

    Bootstrap stuff cleanup

--- test_script.js
+++ test_script.js
@@ -9,7 +9,7 @@ if (!window.tableContentsHash) {
 }
 
 function removeWeeklyBreakdownTable() {
-  let existingTable = document.querySelector(".tableRoot");
+  let existingTable = document.querySelector(".shadowRoot");
   if (existingTable) {
     existingTable.parentNode.removeChild(existingTable);
   }
@@ -40,17 +40,9 @@ function createWeeklyBreakdownTable(scoreData, pointsPerWin, useNascarPoints = t
   // base.appendChild(shadowHost);
   // let shadowRoot = shadowHost.attachShadow({ mode: "open"});
   let shadowRoot = document.createElement("div");
-  shadowRoot.classList.add("tableRoot");
+  shadowRoot.classList.add("shadowRoot");
   base.appendChild(shadowRoot);
   shadowRoot.innerHTML = '<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">';
-  // shadowRoot.innerHTML += '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">';
-  // shadowRoot.innerHTML += '<link href="datatables/mdb.min.css" rel="stylesheet">';
-  // shadowRoot.innerHTML += '<link href="datatables/datatables.min.css" rel="stylesheet">';
-  
-  // shadowRoot.innerHTML += '<script src="//cdnjs.cloudflare.com/ajax/libs/d3/3.4.11/d3.min.js"></script>';
-  shadowRoot.innerHTML += '<script type="text/javascript" src="datatables/jquery-3.4.1.min.js"></script>';
-  shadowRoot.innerHTML += '<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>';
-  shadowRoot.innerHTML += '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>';
   shadowRoot.innerHTML += '<script type="text/javascript" src="datatables/mdb.min.js"></script>';
   shadowRoot.innerHTML += '<script type="text/javascript" src="datatables/datatables.min.js"></script>';
 
