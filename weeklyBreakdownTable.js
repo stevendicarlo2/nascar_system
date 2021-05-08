bfd1cf320dbb17d449f72dbec6d4900acc802af3
Steven DiCarlo <stdicarl@microsoft.com>
2021-05-07 22:35:07 -0700

    Change infrastructure of weekly table data passing

--- weeklyBreakdownTable.js
+++ weeklyBreakdownTable.js
@@ -6,36 +6,54 @@ if (!window.tableContentsHash) {
 class WeeklyBreakdownTable {
   scoreData;
   pointsPerWin;
+  freezeTable;
+  tableElement;
   
   constructor(scoreData, pointsPerWin) {
     this.scoreData = scoreData;
     this.pointsPerWin = pointsPerWin;
+    console.log("abcabc pointsPerWin:", pointsPerWin);
+    this.convertScoreDataToTableStructure();
   }
-
-removeWeeklyBreakdownTable() {
-  let existingTable = document.querySelector("#myDataTable");
-  if (existingTable.innerHTML != "") {
-    existingTable.innerHTML = ""
-  }
-  
-  let existingScoreSwitchLabel = document.querySelector("#scoreTypeSwitchLabel");
-  if (existingScoreSwitchLabel) {
-    existingScoreSwitchLabel.parentNode.removeChild(existingScoreSwitchLabel);
-  }
-  
-  let existingScoreSwitch = document.querySelector("#scoreTypeSwitch");
-  if (existingScoreSwitch) {
-    let scoreSwitchParent = existingScoreSwitch.parentNode;
-    scoreSwitchParent.parentNode.removeChild(scoreSwitchParent);
-  }
-}
 
 recreateWeeklyBreakdownTable(useNascarPoints = true) {
-  this.removeWeeklyBreakdownTable();
-  this.createWeeklyBreakdownTable(useNascarPoints);
+  // this.removeWeeklyBreakdownTable();
+  // this.createWeeklyBreakdownTable(useNascarPoints);
+  console.log("abcabc recreateWeeklyBreakdownTable");
+  let actualTableBefore = document.querySelector("#weekly_breakdown_table");
+  console.log("abcabc actualTableBefore: ", actualTableBefore);  
+  let beforeInfo = actualTableBefore.innerHTML;
+  let actualBeforeInfo = (' ' + beforeInfo).slice(1);
+  // console.log("abcabc actualBeforeInfo: ", actualBeforeInfo);
+
+  this.fillTableWithData(useNascarPoints);
+  this.highlightTable();
+
+  if ($.fn.dataTable.isDataTable('#weekly_breakdown_table')) {
+    console.log("abcabc is already dataTable");
+    let dataTable = $('#weekly_breakdown_table').DataTable();
+    let order = dataTable.order();
+    console.log("abcabc order: ", order);
+    dataTable.destroy();
+  } 
+
+    $('#weekly_breakdown_table').DataTable({
+      "searching": false,
+      "paging": false
+    });
+  // }
+  $('.dataTables_length').addClass('bs-select');
+  // console.log("abcabc tableElement: ", this.tableElement);  
+  let actualTable = document.querySelector("#weekly_breakdown_table");
+  console.log("abcabc actualTable: ", actualTable);  
+  let afterInfo = actualTable.innerHTML;
+  console.log("abcabc innerHTML is same:",  actualBeforeInfo == afterInfo);
+
+  // this.freezeTable.update();
 }
 
 createWeeklyBreakdownTable(useNascarPoints = true) {
+  console.log("abcabc createWeeklyBreakdownTable");
   let shadowRoot = document.querySelector(".shadowRoot");
   let container = shadowRoot.querySelector("#myDataTable");
   
@@ -55,145 +73,95 @@ createWeeklyBreakdownTable(useNascarPoints = true) {
   table.classList.add("table", "table-bordered", "table-sm");
   table.id = "weekly_breakdown_table";
   container.appendChild(table);
-  let thead = document.createElement("thead");
-  table.appendChild(thead);
-  let headRow = document.createElement("tr");
-  thead.appendChild(headRow);
-  let ownerCol = document.createElement("th");
-  ownerCol.classList.add("th-sm");
-  ownerCol.innerHTML = "Team";
-  headRow.appendChild(ownerCol);
-  let totalCol = document.createElement("th");
-  totalCol.classList.add("th-sm");
-  totalCol.innerHTML = "Total NP";
-  headRow.appendChild(totalCol);
-  let adjCol = document.createElement("th");
-  adjCol.classList.add("th-sm");
-  adjCol.innerHTML = "Total ANP";
-  headRow.appendChild(adjCol);
-  for (let week in this.scoreData.weekly_breakdown) {
-    if (week === "storedTime") {
-      continue;
-    }
-    let weekCol = document.createElement("th");
-    weekCol.classList.add("th-sm");
-    weekCol.innerHTML = "Week " + (parseInt(week) + 1).toString();
-    headRow.appendChild(weekCol);
-  }
-
-  // Filling with data
-  let tbody = document.createElement("tbody");
-  table.appendChild(tbody);
-  for (let team in this.scoreData.totals) {
-    let row = document.createElement("tr");
-    let nameEntry = document.createElement("td");
-    nameEntry.classList.add("teamName");
-    nameEntry.innerHTML = team;
-    row.appendChild(nameEntry);
-    let totEntry = document.createElement("td");
-    totEntry.classList.add("NP_data");
-    totEntry.innerHTML = "<b>" + this.scoreData.totals[team].total_NP + "</b>";
-    row.appendChild(totEntry);
-    let adjEntry = document.createElement("td");
-    adjEntry.classList.add("ANP_data");
-    adjEntry.innerHTML = "<b>" + (this.scoreData.totals[team].total_NP + this.scoreData.totals[team].wins*this.pointsPerWin) + "</b>";
-    row.appendChild(adjEntry);
-    for (let week in this.scoreData.weekly_breakdown) {
-      if (week === "storedTime") {
-        continue;
-      }
-      let info = this.scoreData.weekly_breakdown[week][team];
-      let entry = document.createElement("td");
-      entry.setAttribute("data-nascar-points", info.nascar_points + info.wins*this.pointsPerWin);
-      entry.setAttribute("data-raw-points", info.score);
-      entry.setAttribute("data-wins", info.wins);
-      entry.innerHTML = useNascarPoints ? info.nascar_points + info.wins*this.pointsPerWin : info.score;
-      if (info.wins === 1) {
-        entry.innerHTML = "<u>" + entry.innerHTML + "</u>";
-      }
-      entry.classList.add("weeklyData");
-      row.appendChild(entry);
-    }
-    tbody.appendChild(row);
-  }
-  if (useNascarPoints) {
-    container.innerHTML += '<label id="scoreTypeSwitchLabel" for="scoreTypeSwitch">Nascar Points</label><label class="switch"><input id="scoreTypeSwitch" type="checkbox" checked><span class="slider round"></span></label>';
-  } else {
-    container.innerHTML += '<label id="scoreTypeSwitchLabel" for="scoreTypeSwitch">Weekly Score</label><label class="switch"><input id="scoreTypeSwitch" type="checkbox"><span class="slider round"></span></label>';
-  }
-  
 
   $(document).ready(() => {
+    let tableData = this.convertScoreDataToTableStructure();
+    let columns = this.createColumnDefinitionsForTable();
     if ($.fn.dataTable.isDataTable('#weekly_breakdown_table')) {
       $('#weekly_breakdown_table').DataTable();
     } else {
       $('#weekly_breakdown_table').DataTable({
-        "searching": false,
-        "paging": false,
-        "order": [[ 2, "desc" ]]
+        searching: false,
+        paging: false,
+        data: tableData,
+        columns: columns
       });
     }
-    $('.dataTables_length').addClass('bs-select');
-    $('#weekly_breakdown_table').parent().addClass('freezeTable');
-    var freezeTable = new FreezeTable('.freezeTable', {
-      "columnNum": 3, 
-      "columnWrapStyles": {'border-right': '3px solid black'},
-      "freezeColumnHead": false,
-      "freezeHead": false
-    });
+    // $('.dataTables_length').addClass('bs-select');
+    // this.highlightTable();
+  });
+}
 
-    let addHandlerToBreakdownHeader = (header, isOriginal) => {
-      if (header.getAttribute("sort_handler_exists") !== "true") {
-        header.setAttribute("sort_handler_exists", "true");
-        header.addEventListener("click", () => {
-          if (!isOriginal) {
-            let originalHeader = $( "#weekly_breakdown_table th:contains('" + header.textContent + "')" );
-            originalHeader.click();
-          }
-          let oldTableCopy = document.querySelector("#weekly_breakdown_table_wrapper .clone-column-table-wrap table");
-          let oldTableStyle = oldTableCopy.style.cssText;
-          let newTableCopy = document.getElementById("weekly_breakdown_table").cloneNode(true);
-          newTableCopy.removeAttribute("id");
-          newTableCopy.style.cssText = oldTableStyle;
-          newTableCopy.querySelectorAll("th").forEach((header) => {
-            header.removeAttribute("sort_handler_exists");
-          });
-
-          let tableParent = oldTableCopy.parentNode;
-          tableParent.removeChild(oldTableCopy);
-          tableParent.appendChild(newTableCopy);
-          addHandlersToAllBreakdownHeaders();
-        });
+createColumnDefinitionsForTable() {
+  let columns = [
+    { data: "name" },
+    { 
+      data: "Total NP", 
+      render: (data, type, row, meta) => {
+        return this.renderMethodTotalField(row, false, type)
+      }
+    },
+    { 
+      data: "Total ANP", 
+      render: (data, type, row, meta) => {
+        return this.renderMethodTotalField(row, true, type)
       }
     }
+  ]
 
-    let addHandlersToAllBreakdownHeaders = () => {
-      let copiedHeaders = document.querySelectorAll('#weekly_breakdown_table_wrapper .clone-column-table-wrap th');
-      copiedHeaders.forEach((header) => {
-        addHandlerToBreakdownHeader(header, false);
-      });
-      let originalHeaders = document.querySelectorAll('#weekly_breakdown_table th');
-      originalHeaders.forEach((header) => {
-        addHandlerToBreakdownHeader(header, true);
-      });
+  for (let week in this.scoreData.weekly_breakdown) {
+    if (week === "storedTime") {
+      continue;
     }
-    addHandlersToAllBreakdownHeaders();
-    let targetNode = document.querySelector('#weekly_breakdown_table_wrapper .clone-column-table-wrap');
-    let observer = new MutationObserver(() => {
-        if(targetNode.style.visibility === 'visible') {
-          addHandlersToAllBreakdownHeaders();
-        }
-    });
-    observer.observe(targetNode, { attributes: true, childList: true });
+    let weekColumnObject = {
+      data: "Week " + (week + 1),
+      render: (data, type, row, meta) => {
+        return this.renderMethodWeekField(row, week, type)
+      }
+    }
+    columns.push(weekColumnObject);
+  }
 
-    let scoreTypeSwitch = $("#scoreTypeSwitch");
-    scoreTypeSwitch.click(() => {
-      let checked = scoreTypeSwitch.prop("checked");
-      $("#scoreTypeSwitchLabel").html(checked ? "Nascar Points" : "Weekly Score");
-      this.recreateWeeklyBreakdownTable(checked);
-    });
-    this.highlightTable();
-  });
+  return columns;
+}
+
+renderMethodTotalField(teamData, useAdjustedPoints, type) {
+  let total = 0;
+  for (let week in teamData.weeklyInfo) {
+    let weekData = teamData.weeklyInfo[week];
+    if (useAdjustedPoints) {
+      total += weekData.nascar_points + weekData.wins * this.pointsPerWin;
+    }
+    else {
+      total += weekData.nascar_points;
+    }
+  }
+  return total;
+}
+
+renderMethodWeekField(teamData, week, type) {
+  return teamData.weeklyInfo[week].nascar_points
+}
+
+convertScoreDataToTableStructure() {
+  let allDataRows = [];
+  let weekly_breakdown = this.scoreData.weekly_breakdown;
+  for (let team in this.scoreData.totals) {
+    let teamEntry = {};
+    teamEntry.name = team;
+    let weeklyInfo = [];
+    for (let week in weekly_breakdown) {
+      if (week === "storedTime") {
+        continue;
+      }
+      let weekData = weekly_breakdown[week][team];
+      weeklyInfo.push(weekData);
+    }
+    teamEntry.weeklyInfo = weeklyInfo;
+    allDataRows.push(teamEntry);
+  }
+  console.log("abcabc allDataRows: ", allDataRows);
+  return allDataRows
 }
 
 highlightTable() {
